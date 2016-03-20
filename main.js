1b2af97831fdb98ef9a35b17758b87ea8f850f55
dylanOshima <2017.dylan.oshima@isak.jp>
2016-03-20 16:04:35 +0900

    Reall adds all the website files... Dumbass

---
+++ main.js
@@ -0,0 +1,25 @@
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+
+var count = 1;
+
+function updateCount() {
+    count = checkTime(count);
+    document.getElementById('count').innerHTML = count;
+    var z = setTimeout(function(){count++; updateCount();}, 100000);
+}
